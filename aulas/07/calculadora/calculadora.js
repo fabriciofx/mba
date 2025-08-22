39f1075abe5c947d1e32dc9cb3cd955c0bcf58d9
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-08-22 15:33:12 -0300

    Move a criação da mensagem no case de cada operação da calculadora

--- aulas/07/calculadora/calculadora.js
+++ aulas/07/calculadora/calculadora.js
@@ -3,19 +3,34 @@ const rls = require("readline-sync");
 const num1 = Number(rls.question("Digite um número: "));
 const num2 = Number(rls.question("Digite outro número: "));
 const op = rls.question("Digite a operação (+, -, * ou /): ");
-let result = "";
+let msg;
 switch(op) {
-  case '+':
-      result = num1 + num2;
-      break;
-  case '-':
-      result = num1 - num2;
-      break;
-  case '*':
-      result = num1 * num2;
-      break;
-  case '/':
-      result = num1 / num2;
-      break;
+  case "+": {
+      const result = num1 + num2;
+      msg = `A soma entre ${num1} e ${num2} é ${result}`;
+    }
+    break;
+  case "-": {
+      const result = num1 - num2;
+      msg = `A subtração entre ${num1} e ${num2} é ${result}`;
+    }
+    break;
+  case "*": {
+      const result = num1 * num2;
+      msg = `A multiplicação entre ${num1} e ${num2} é ${result}`;
+    }
+    break;
+  case "/": {
+      const result = num1 / num2;
+      if (num2 === 0) {
+        msg = `Erro: não é possível realizar a divisão de ${num1} por zero!`;
+      } else {
+        msg = `A divisão entre ${num1} e ${num2} é ${result}`;
+      }
+    }
+    break;
+  default:
+    msg = `Operação ${op} é inválida. Informe uma operação válida (+, -, * ou /)`;
+    break;
 }
-console.log(`${num1} ${op} ${num2} = ${result}`)
+console.log(msg)
