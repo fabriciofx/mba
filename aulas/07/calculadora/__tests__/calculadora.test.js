62e71263922ed7cd0a056a73e0b847228e21a18e
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-08-22 16:36:15 -0300

    Cria testes automatizados para a calculadora em js

---
+++ aulas/07/calculadora/__tests__/calculadora.test.js
@@ -0,0 +1,44 @@
+import { Calculadora } from "../src/calculadora.js"
+
+test(
+  "somar 2 e 3 deve dar 5",
+  () => {
+    expect(new Calculadora(2, 3).soma()).toBe("A soma entre 2 e 3 é 5");
+  }
+);
+
+test(
+  "multiplicar 2 e 3 deve dar 6",
+  () => {
+    expect(new Calculadora(2, 3).mult()).toBe(
+      "A multiplicação entre 2 e 3 é 6"
+    );
+  }
+);
+
+test(
+  "subtrair 5 e 2 deve dar 3",
+  () => {
+    expect(new Calculadora(5, 2).sub()).toBe(
+      "A subtração entre 5 e 2 é 3"
+    );
+  }
+);
+
+test(
+  "Dividir 3 e 2 deve dar 1.5",
+  () => {
+    expect(new Calculadora(3, 2).div()).toBe(
+      "A divisão entre 3 e 2 é 1.5"
+    );
+  }
+);
+
+test(
+  "Dividir 2 e 3 deve dar 0.6666666666666666",
+  () => {
+    expect(new Calculadora(2, 3).div()).toBe(
+      "A divisão entre 2 e 3 é 0.6666666666666666"
+    );
+  }
+);
