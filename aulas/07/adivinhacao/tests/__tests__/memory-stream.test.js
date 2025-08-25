4bfdea4f7ff3df6448e5989113d36fbaa5cc0ece
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-08-25 01:20:26 -0300

    Modifica MemoryStream para ser um buffer contínuo

--- aulas/07/adivinhacao/tests/__tests__/memory-stream.test.js
+++ aulas/07/adivinhacao/tests/__tests__/memory-stream.test.js
@@ -4,7 +4,105 @@ test(
   "Escreve no stream",
   () => {
     const msg = "Olá, mundo!";
-    const stream = new MemoryStream({data: [msg]});
+    const stream = new MemoryStream();
+    stream.write(msg);
     expect(stream.toString()).toBe(msg);
+  }
+);
+
+test(
+  "Escreve no stream duas vezes seguidas",
+  () => {
+    const msg1 = "Olá, mundo!";
+    const msg2 = "A vida é bela!";
+    const stream = new MemoryStream();
+    stream.write(msg1);
+    stream.write(msg2);
+    expect(stream.toString()).toBe(msg1 + msg2);
+  }
+);
+
+test(
+  "Lê no stream",
+  () => {
+    const msg = "Olá, mundo!";
+    const stream = new MemoryStream({data: [msg]});
+    const data = stream.read().toString();
+    expect(data).toBe(msg);
+  }
+);
+
+test(
+  "Lê no stream dois dados",
+  () => {
+    const msg1 = "Olá, mundo!";
+    const msg2 = "A vida é bela!";
+    const stream = new MemoryStream({data: [msg1, msg2]});
+    const data = stream.read().toString();
+    expect(data).toBe(msg1 + msg2);
+  }
+);
+
+test(
+  "Verifica o tamanho quando vazio",
+  () => {
+    const stream = new MemoryStream();
+    expect(stream.size()).toBe(0);
+  }
+);
+
+test(
+  "Lê null quando tenta ler de um do vazio",
+  () => {
+    const stream = new MemoryStream();
+    expect(stream.read()).toBe(null);
+  }
+);
+
+test(
+  "Verifica o quanto foi escrito na memória",
+  () => {
+    const msg1 = "Olá, mundo!";
+    const msg2 = "A vida é bela!";
+    const length = Buffer.from(msg1).length;
+    const stream = new MemoryStream();
+    stream.write(msg1);
+    expect(stream.size()).toBe(length);
+    stream.write(msg2);
+    expect(stream.size()).toBe(length + Buffer.from(msg2).length);
+  }
+);
+
+test(
+  "Verifica o tamanho quando dados são passados no construtor",
+  () => {
+    const msg = "Olá, mundo!";
+    const length = Buffer.from(msg).length;
+    const stream = new MemoryStream({data: [msg]});
+    expect(stream.size()).toBe(length);
+  }
+);
+
+test(
+  "Verifica se zera o tamanho após ler todo o conteúdo",
+  () => {
+    const msg = "Olá, mundo!";
+    const length = Buffer.from(msg).length;
+    const stream = new MemoryStream({data: [msg]});
+    const data = stream.read();
+    expect(stream.size()).toBe(0);
+  }
+);
+
+test(
+  "Verifica o tamanho após ler parcialmente o conteúdo",
+  () => {
+    const msg1 = "Olá, mundo!";
+    const msg2 = "A vida é bela!";
+    const length1 = Buffer.from(msg1).length;
+    const length2 = Buffer.from(msg2).length;
+    const stream = new MemoryStream({data: [msg1, msg2]});
+    stream.read(length1);
+    expect(stream.size()).toBe(length2);
   }
 );
--- aulas/07/adivinhacao/tests/helpers/memory-stream.js
+++ aulas/07/adivinhacao/tests/helpers/memory-stream.js
@@ -1,25 +1,101 @@
-import { Writable } from "stream";
+import { Duplex } from "stream";
 
-export class MemoryStream extends Writable {
-  #chunks;
+class FixedBuffer {
+  #data;
+  #offset;
+  #extra;
 
-  constructor({options, data = []} = {}) {
-    super(options);
-    this.#chunks = data.map(datum => Buffer.from(datum, "utf8"));
+  constructor(data = []) {
+    this.#data = Buffer.concat(data.map(datum => Buffer.from(datum, "utf8")));
+    this.#offset = 0;
+    this.#extra = Buffer.alloc(0);
   }
 
-  _write(chunk, encoding, callback) {
-    let buffer;
+  write(chunk, encoding, callback) {
     if (Buffer.isBuffer(chunk)) {
-      buffer = chunk;
+      this.#data = Buffer.concat([this.#data, chunk]);
     } else {
-      buffer = Buffer.from(chunk, encoding);
+      this.#data = Buffer.concat(
+        [
+          this.#data,
+          Buffer.from(chunk, encoding)
+        ]
+      );
     }
-    this.#chunks.push(buffer);
     callback();
   }
 
+  read(size) {
+    size = size ?? 65536;
+    if (this.#extra.length >= size) {
+      const chunk = this.#extra.subarray(0, size);
+      this.#extra = this.#extra.subarray(size);
+      return chunk;
+    }
+    const remainingSize = size - this.#extra.length;
+    const remainingData = this.#data.length - this.#offset;
+    if (remainingData <= 0 && this.#extra.length === 0) {
+      return null;
+    }
+    const take = Math.min(remainingSize, remainingData);
+    const chunk = Buffer.concat(
+      [
+        this.#extra,
+        this.#data.subarray(this.#offset, this.#offset + take)
+      ]
+    );
+    this.#offset += take;
+    if (chunk.length > size) {
+      this.#extra = chunk.subarray(size);
+      return chunk.subarray(0, size);
+    } else {
+      this.#extra = Buffer.alloc(0);
+    }
+    return chunk;
+  }
+
+  size() {
+    return this.#data.length - this.#offset;
+  }
+
+  content() {
+    return this.#data;
+  }
+
   toString(encoding = "utf8") {
-    return Buffer.concat(this.#chunks).toString(encoding);
+    return this.content().toString(encoding);
+  }
+}
+
+export class MemoryStream extends Duplex {
+  #buffer;
+
+  constructor({options, data = []} = {}) {
+    super(options);
+    this.#buffer = new FixedBuffer(data);
+  }
+
+  _write(chunk, encoding, callback) {
+    this.#buffer.write(chunk, encoding, callback);
+  }
+
+  _read(size) {
+    this.push(this.#buffer.read(size));
+  }
+
+  read(size) {
+    return this.#buffer.read(size);
+  }
+
+  size() {
+    return this.#buffer.size();
+  }
+
+  content() {
+    return this.#buffer.content()
+  }
+
+  toString(encoding = "utf8") {
+    return this.#buffer.toString(encoding);
   }
 }
