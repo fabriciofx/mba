1146d70d9f068aaacbed7e061d26290494e56672
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-08-25 20:35:20 -0300

    Remove a classe FixedBuffer da MemoryStream

--- aulas/07/adivinhacao/tests/helpers/memory-stream.js
+++ aulas/07/adivinhacao/tests/helpers/memory-stream.js
@@ -26,79 +26,13 @@ class MemoryArea {
     this.#offset = this.#offset + chunk.length;
     return chunk;
   }
+
   size() {
     return this.#area.length - this.#offset;
   }
 
   content() {
     return this.#area;
-  }
-
-  toString(encoding = "utf8") {
-    return this.content().toString(encoding);
-  }
-}
-
-class FixedBuffer {
-  #data;
-  #offset;
-  #extra;
-
-  constructor(data = []) {
-    this.#data = Buffer.concat(data.map(datum => Buffer.from(datum, "utf8")));
-    this.#offset = 0;
-    this.#extra = Buffer.alloc(0);
-  }
-
-  write(chunk, encoding, callback) {
-    if (Buffer.isBuffer(chunk)) {
-      this.#data = Buffer.concat([this.#data, chunk]);
-    } else {
-      this.#data = Buffer.concat(
-        [
-          this.#data,
-          Buffer.from(chunk, encoding)
-        ]
-      );
-    }
-    callback();
-  }
-
-  read(size) {
-    size = size ?? 65536;
-    if (this.#extra.length >= size) {
-      const chunk = this.#extra.subarray(0, size);
-      this.#extra = this.#extra.subarray(size);
-      return chunk;
-    }
-    const remainingSize = size - this.#extra.length;
-    const remainingData = this.#data.length - this.#offset;
-    if (remainingData <= 0 && this.#extra.length === 0) {
-      return null;
-    }
-    const take = Math.min(remainingSize, remainingData);
-    const chunk = Buffer.concat(
-      [
-        this.#extra,
-        this.#data.subarray(this.#offset, this.#offset + take)
-      ]
-    );
-    this.#offset += take;
-    if (chunk.length > size) {
-      this.#extra = chunk.subarray(size);
-      return chunk.subarray(0, size);
-    } else {
-      this.#extra = Buffer.alloc(0);
-    }
-    return chunk;
-  }
-
-  size() {
-    return this.#data.length - this.#offset;
-  }
-
-  content() {
-    return this.#data;
   }
 
   toString(encoding = "utf8") {
