32bf5b51355da57b95c1452ae4916dcc8f585df6
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-08-26 00:06:59 -0300

    Adiciona mais testes ao Palpite

--- aulas/07/adivinhacao/tests/__tests__/palpite.tests.js
+++ aulas/07/adivinhacao/tests/__tests__/palpite.tests.js
@@ -11,12 +11,50 @@ test(
     const palpite = new Palpite(console);
     const contexto = palpite.turno(
       {
-        min: () => 10, max: () => 90, secreto: () => 42
+        min: () => 10,
+        max: () => 90,
+        secreto: () => 42
       }
     );
     expect(contexto.min()).toBe(10);
     expect(contexto.max()).toBe(57);
     expect(contexto.secreto()).toBe(42);
     expect(contexto.numero()).toBe(57);
+ }
+);
+
+test(
+  "Deve exibir ao usuário: \'Adivinhe um número entre 1 e 100:\'",
+  () => {
+    const input = new MemoryStream({data: ["57"]});
+    const output = new MemoryStream();
+    const console = new Console({input: input, output: output});
+    const palpite = new Palpite(console);
+    const contexto = palpite.turno(
+      {
+        min: () => 1,
+        max: () => 100,
+        secreto: () => 42
+      }
+    );
+    expect(output.toString()).toBe("Adivinhe um número entre 1 e 100: ");
+ }
+);
+
+test(
+  "Deve exibir ao usuário: \'Adivinhe um número entre 10 e 90:\'",
+  () => {
+    const input = new MemoryStream({data: ["57"]});
+    const output = new MemoryStream();
+    const console = new Console({input: input, output: output});
+    const palpite = new Palpite(console);
+    const contexto = palpite.turno(
+      {
+        min: () => 10,
+        max: () => 90,
+        secreto: () => 42
+      }
+    );
+    expect(output.toString()).toBe("Adivinhe um número entre 10 e 90: ");
  }
 );
