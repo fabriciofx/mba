a8ce61967b28db92f857546b3bd1016b4f8394f9
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-08-26 12:45:38 -0300

    Adiciona teste para verificar se o número secreto das tentativas está correto

--- aulas/07/adivinhacao/tests/__tests__/tentativas.tests.js
+++ aulas/07/adivinhacao/tests/__tests__/tentativas.tests.js
@@ -17,3 +17,21 @@ test(
     expect(tentativas.secreto()).toBe(secreto);
   }
 );
+
+test(
+  "Deve verificar se o número secreto está correto",
+  () => {
+    const secreto = 42;
+    const palpite = {
+      anterior: () => ({
+        secreto: () => secreto
+      })
+    };
+    const avaliacao = {
+      palpite: () => palpite,
+      igual: () => false
+    };
+    const tentativas = new Tentativas(avaliacao, 5);
+    expect(tentativas.secreto()).toBe(secreto);
+  }
+);
